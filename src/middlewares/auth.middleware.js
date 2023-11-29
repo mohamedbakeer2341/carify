a953aa3d367b4092576e02479ddabb6c740509ab
mohamedbakeer2341 <mohamedbakeer2341@gmail.com>
2023-11-29 03:50:55 +0200

    remove bearer

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,8 +5,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const authenticate = asyncHandler(async(req,res,next)=>{
     let token = req.headers.token
-    if(!token || !token.startsWith(process.env.BEARER_TOKEN)) return next(new Error("Invalid token"))
-    token = token.split(process.env.BEARER_TOKEN)[1]
+    if(!token) return next(new Error("Invalid token"))
     const tokenDB = await Token.findOne({token,isValid:true})
     if(!tokenDB) return next(new Error("Invalid or expired token",{cause:401}))
     const decoded = jwt.verify(token,process.env.SECRET_KEY)
