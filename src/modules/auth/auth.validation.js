88bfd0078b286c7da8e7c16a7d25398cfe4a7f22
mohamedbakeer2341 <mohamedbakeer2341@gmail.com>
2023-11-29 16:50:05 +0200

    fix email message

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -3,7 +3,7 @@ import joi from "joi";
 export const signUpSchema = joi.object({
     firstName:joi.string().required().min(3).max(30),
     lastName:joi.string().required().min(3).max(30),
-    email:joi.string().email({minDomainSegments:2,tlds:{allow:["com","net"]}}).lowercase().required(),
+    email:joi.string().email({minDomainSegments:2,tlds:{allow:["com","net"]}}).lowercase().required().messages({"any.only":"Email domain must have at least 2 segments and ends with 'com' or 'net"}),
     password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required().messages({"string.pattern.base":"Password must be at least 8 characters long"}),
     cPassword: joi.string().valid(joi.ref("password")).required().messages({"any.only":"Passwords do not match !"}),
     role: joi.string().valid("admin","user").required(),
