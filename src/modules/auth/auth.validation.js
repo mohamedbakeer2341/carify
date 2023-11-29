b748727aa8d0bd629c3e4ee404ed7f25c3b7798b
mohamedbakeer2341 <mohamedbakeer2341@gmail.com>
2023-11-29 16:28:53 +0200

    password pattern

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -4,7 +4,7 @@ export const signUpSchema = joi.object({
     firstName:joi.string().required().min(3).max(30),
     lastName:joi.string().required().min(3).max(30),
     email:joi.string().email({minDomainSegments:2,tlds:{allow:["com","net"]}}).lowercase().required(),
-    password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required(),
+    password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required().messages({"string.pattern.base":"Password must be at least 8 characters long"}),
     cPassword: joi.string().valid(joi.ref("password")).required().messages({"any.only":"Passwords do not match !"}),
     role: joi.string().valid("admin","user").required(),
     gender: joi.string().valid("male","female").required(),
@@ -25,7 +25,7 @@ export const sendForgetCodeSchema = joi.object({
 
 export const resetPasswordSchema = joi.object({
     email:joi.string().email({minDomainSegments:2,tlds:{allow:["com","net"]}}).lowercase().required(),
-    password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required(),
+    password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required().messages({"string.pattern.base":"Password must be at least 8 characters long"}),
     cPassword: joi.string().valid(joi.ref("password")).required().messages({"any.only":"Passwords do not match !"}),
     forgetCode:joi.string().required()
 }).required()
