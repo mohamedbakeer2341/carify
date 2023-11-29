b31979e7b1cab072471459d1c4eb48a23390a40a
mohamedbakeer2341 <mohamedbakeer2341@gmail.com>
2023-11-29 15:40:33 +0200

    changed url slug

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -7,15 +7,15 @@ import { upload } from "../../utils/multer.js";
 
 const router = Router()
 
-router.post('/sign_up',validate(signUpSchema),signUp)
+router.post('/sign-up',validate(signUpSchema),signUp)
 router.get('/activation/:activationCode',validate(activateAccountSchema),activateAccount)
 router.post('/login',validate(loginSchema),login)
-router.patch('/forgot_password',validate(sendForgetCodeSchema),sendForgetCode)
-router.post('/reset_password',validate(resetPasswordSchema),resetPassword)
-router.post('/change_password',validate(changePasswordSchema),authenticate,changePassword)
-router.patch('/account_details',validate(changeAccountDetailsSchema),authenticate,changeAccountDetails)
+router.patch('/forgot-password',validate(sendForgetCodeSchema),sendForgetCode)
+router.post('/reset-password',validate(resetPasswordSchema),resetPassword)
+router.post('/change-password',validate(changePasswordSchema),authenticate,changePassword)
+router.patch('/account-details',validate(changeAccountDetailsSchema),authenticate,changeAccountDetails)
 router.get('/profile',authenticate,getProfile)
-router.patch('/profile_picture',upload().single('pp'),authenticate,uploadProfilePicture)
+router.patch('/profile-picture',upload().single('pp'),authenticate,uploadProfilePicture)
 router.patch('/logout',authenticate,signOut)
 
 export default router
