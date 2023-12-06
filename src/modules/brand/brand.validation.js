94b8df7ce87f5fbff8399241cc8e8b6892969971
mohamedbakeer2341 <mohamedbakeer2341@gmail.com>
2023-12-06 02:18:23 +0200

    remove info and country is required

--- src/modules/brand/brand.validation.js
+++ src/modules/brand/brand.validation.js
@@ -3,15 +3,13 @@ import joi from "joi"
 export const addBrandSchema = joi.object({
     name: joi.string().min(3).max(30).required(),
     logo: joi.string().required(),
-    info: joi.string(),
-    country: joi.string(),
+    country: joi.string().required(),
 }).required()
 
 export const editBrandSchema = joi.object({
     id: joi.string().required(),
     name: joi.string().min(3).max(30),
     logo: joi.string(),
-    info: joi.string(),
     country: joi.string()
 }).required()
 
